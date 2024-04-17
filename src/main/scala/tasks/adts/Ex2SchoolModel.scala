679fca8f55c2cddccdc3f9b07bbbf4f5f75fb86e
Valerio-Giannini <valerio.giannini@studio.unibo.it>
2024-04-17 12:53:01 +0200

    Task 2: end of model -> need to test

--- src/main/scala/tasks/adts/Ex2SchoolModel.scala
+++ src/main/scala/tasks/adts/Ex2SchoolModel.scala
@@ -1,7 +1,11 @@
 package tasks.adts
 import u03.Sequences.*
+import u03.Sequences.Sequence.*
 import u03.Optionals.*
+import u03.Optionals.Optional.*
 import u02.AlgebraicDataTypes.Person
+
+import scala.annotation.tailrec
 
 /*  Exercise 2: 
  *  Implement the below trait, and write a meaningful test.
@@ -32,14 +36,43 @@ object SchoolModel:
   object BasicSchoolModule extends SchoolModule:
     type School = (Sequence[Teacher], Sequence[Course])
     type Teacher = (String, Sequence[Course])
-    type Course = (String)
+    type Course = String
 
     extension (school: School)
-      def addTeacher(name: String): School = ???
-      def addCourse(name: String): School = ???
-      def teacherByName(name: String): Optional[Teacher] = ???
-      def courseByName(name: String): Optional[Course] = ???
-      def nameOfTeacher(teacher: Teacher): String = ???
-      def nameOfCourse(teacher: Teacher): String = ???
-      def setTeacherToCourse(teacher: Teacher, course: Course): School = ???
-      def coursesOfATeacher(teacher: Teacher): Sequence[Course] = ???
+      def addTeacher(name: String): School =
+        var t: Teacher = (name, Nil())
+        (Cons(t, Nil()), //Sequence of Teacher
+          Nil())         //sequence of Course
+
+      def addCourse(name: String): School =
+        (Nil(),              //Sequence of teacher
+          Cons(name, Nil())) //Sequence of Course
+
+      @tailrec
+      def loop[A](s: Sequence[A])(name: String): Optional[A]  = s match
+        case Cons(h, t) => h match
+          case (n, _) if n == name => Just(h)
+          case (n, _) if n != name => loop(t)(name)
+        case Nil() => Empty()
+
+      def teacherByName(name: String): Optional[Teacher] = school match
+        case (st, _) => loop(st)(name)
+
+      def courseByName(name: String): Optional[Course] = school match
+        case (_, sc) =>  loop(sc)(name)
+
+      def nameOfTeacher(teacher: Teacher): String = teacher match
+        case (n, _) => n
+
+      def nameOfCourse(teacher: Teacher): String = teacher match
+        case (_, c) => c match
+          case Cons(h, Nil()) => h
+
+      def setTeacherToCourse(teacher: Teacher, course: Course): School =
+        var t: Teacher = teacher
+        t match
+          case (_, c) => Cons(course, c)
+        (Cons(t, Nil()), Cons(course, Nil()))
+
+      def coursesOfATeacher(teacher: Teacher): Sequence[Course] = teacher match
+        case (_, sc) => sc
--- src/test/scala/tasks/adts/SchoolModelTests.scala
+++ src/test/scala/tasks/adts/SchoolModelTests.scala
@@ -2,13 +2,26 @@ package tasks.adts
 
 import org.junit.*
 import org.junit.Assert.*
-import tasks.adts.Ex1ComplexNumbers.*
+import tasks.adts.SchoolModel.*
 import tasks.adts.SchoolModel.BasicSchoolModule
+import tasks.adts.SchoolModel.BasicSchoolModule.*
 import u03.Sequences.*
+import u03.Sequences.Sequence.*
 import u03.Optionals.*
+import u03.Optionals.Optional.*
 
 class SchoolTests:
-  import BasicSchoolModule.*
+  val schoolModule: SchoolModule = BasicSchoolModule
+
+  val course1: Course = "pps"
+  val course2: Course = "ppc"
+  val course3: Course = "Cyber"
+
+  val t1: Teacher = ( "Viroli", Cons(course1, Nil()))
+  val t2: Teacher = ( "Ricci", Cons(course2, Nil()))
+  val t3: Teacher = ( "D'Angelo", Cons(course3, Nil()))
+
+  val school: School = (Cons(t1, Cons(t2, Cons(t3, Nil()))), Cons(course1, Cons(course2, Cons(course3, Nil()))))
 
 @Test def addTeacher() = ???
 @Test def addCourse() = ???
