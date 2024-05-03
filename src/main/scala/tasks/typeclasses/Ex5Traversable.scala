1ca5d0e08b4da4c04900937969ac0d7cc062dc03
Valerio-Giannini <valerio.giannini@studio.unibo.it>
2024-05-03 15:37:21 +0200

    Task 5: End

--- src/main/scala/tasks/typeclasses/Ex5Traversable.scala
+++ src/main/scala/tasks/typeclasses/Ex5Traversable.scala
@@ -2,6 +2,9 @@ package u04lab
 import u03.Sequences.* 
 import Sequence.*
 
+import u03.Optionals.*
+import Optional.*
+
 /*  Exercise 5: 
  *  - Generalise by ad-hoc polymorphism logAll, such that:
  *  -- it can be called on Sequences but also on Optional, or others... 
@@ -23,4 +26,27 @@ object Ex5Traversable:
     case Cons(h, t) => log(h); logAll(t)
     case _ => ()
 
+  trait Traversable[T[_]]:
+    def traverse[A](ta: T[A], f: A => Unit): Unit
+
+  given Traversable[Optional] with
+    def traverse[A](opt: Optional[A], f: A => Unit): Unit = opt match
+      case Just(a) => f(a)
+      case Empty() => ()
   
+  given Traversable[Sequence] with
+    def traverse[A](seq: Sequence[A], f: A => Unit): Unit = seq match
+      case Cons(h, t) => f(h); traverse(t, f)
+      case _ => ()
+
+  object Ex5Traversable:
+    def log[A](a: A): Unit = println("The next element is: " + a)
+
+    def logAll[T[_] : Traversable, A](data: T[A]): Unit =
+      summon[Traversable[T]].traverse(data, log)
+
+  val optionalData: Optional[Int] = Just(10)
+  Ex5Traversable.logAll(optionalData)
+
+  val sequenceData: Sequence[Int] = Cons(1, Cons(2, Cons(3, Nil())))
+  Ex5Traversable.logAll(sequenceData)
